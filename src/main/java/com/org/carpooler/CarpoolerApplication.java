2aad78dec483e822639b8dcb981a00d1c1f08371
Rajat <rajat@mantralabsglobal.com>
2018-07-21 17:04:31 +0530

    and new repo

---
+++ src/main/java/com/org/carpooler/CarpoolerApplication.java
@@ -0,0 +1,12 @@
+package com.org.carpooler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolerApplication.class, args);
+    }
+}
